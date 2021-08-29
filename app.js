d69a25d115a675de0a20b4fecf24d49c8949098a
Ninewinger <d.v.petricio@live.com>
2021-08-29 22:36:59 +0000

    aun sin funcionar

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ window.onload = function () {
         if (num === 4) return "♣";
 
     }
-    function Array_cartas(num) {
+    function array_cartas(num) {
+        let array_cartas = []
         for (let i = 0; i <= num - 1; i++) {
 
             let num_y_letras = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
@@ -73,9 +74,7 @@ window.onload = function () {
             pinta2_p.setAttributeNode(id_pinta2);
 
             numero_p.innerHTML = genera_letra(num_y_letras[random_numero]);
-            arr_numero.push(num_y_letras[random_numero]);
             let pintara = genera_pinta(pintas[random_pinta]);
-            arr_pinta.push(random_pinta)
             pinta1_p.innerHTML = pintara;
             pinta2_p.innerHTML = pintara;
 
@@ -90,115 +89,57 @@ window.onload = function () {
             carta.appendChild(pinta1_div);
             carta.appendChild(numero_div);
             carta.appendChild(pinta2_div);
-            contenedor_cartas.appendChild(carta);
-        }
-
-        return carta
-    }
-    function Array_cartas_dadas(num, pinta) {
-        for (let i = 0; i <= num.length - 1; i++) {
-
-            let name_carta = document.createAttribute("name");
-            name_carta.value = "carta"
-            let cl_carta = document.createAttribute("class");
-            cl_carta.value = "d-flex justify-content-between m-3";
-            let id_carta = document.createAttribute("id");
-            id_carta.value = "carta";
-            let cl_pinta1 = document.createAttribute("class");
-            cl_pinta1.value = "display-5";
-            let id_pinta1 = document.createAttribute("id");
-            id_pinta1.value = "pinta1";
-            let cl_numero_div = document.createAttribute("class");
-            cl_numero_div.value = "align-self-center"
-            let cl_numero = document.createAttribute("class");
-            cl_numero.value = "display-1"
-            let id_numero = document.createAttribute("id");
-            id_numero.value = "numero"
-            let cl_pinta2_div = document.createAttribute("class");
-            cl_pinta2_div.value = "align-self-end"
-            let cl_pinta2 = document.createAttribute("class");
-            cl_pinta2.value = "display-5"
-            let id_pinta2 = document.createAttribute("id");
-            id_pinta2.value = "pinta2"
-
-            let carta = document.createElement("div");
-            carta.setAttributeNode(cl_carta);
-            carta.setAttributeNode(id_carta);
-            carta.setAttributeNode(name_carta)
-            let pinta1_div = document.createElement("div");
-            let pinta1_p = document.createElement("p");
-            pinta1_p.setAttributeNode(cl_pinta1);
-            pinta1_p.setAttributeNode(id_pinta1);
-            let numero_div = document.createElement("div");
-            numero_div.setAttributeNode(cl_numero_div);
-            let numero_p = document.createElement("p");
-            numero_p.setAttributeNode(cl_numero);
-            numero_p.setAttributeNode(id_numero);
-            let pinta2_div = document.createElement("div");
-            pinta2_div.setAttributeNode(cl_pinta2_div);
-            let pinta2_p = document.createElement("p");
-            pinta2_p.setAttributeNode(cl_pinta2);
-            pinta2_p.setAttributeNode(id_pinta2);
-
-            numero_p.innerHTML = genera_letra(num[i]);
+            array_cartas.push(carta)
             
-            let pintara = genera_pinta(pinta[i]);
-            
-            pinta1_p.innerHTML = pintara;
-            pinta2_p.innerHTML = pintara;
-
-            if (pintara == "♦" || pintara == "♥") {
-                pinta1_p.classList.add("text-danger");
-                pinta2_p.classList.add("text-danger");
-            }
-
-            pinta1_div.appendChild(pinta1_p);
-            numero_div.appendChild(numero_p);
-            pinta2_div.appendChild(pinta2_p);
-            carta.appendChild(pinta1_div);
-            carta.appendChild(numero_div);
-            carta.appendChild(pinta2_div);
-            bubble_log.appendChild(carta);
         }
-
-        return carta
+        
+        return array_cartas
     }
-
-    let arr_numero = []
-    let arr_pinta = []
-    console.log(arr_numero)
-    console.log(arr_pinta)
-
+    let mazo_glob = []
+    console.log(mazo_glob)
+    let cantidad_glob = []
 
     boton_generador.addEventListener("click", function () {
         contenedor_cartas.innerHTML = "";
         let cantidad_cartas = numero_cartas.value;
-        let cartas = Array_cartas(cantidad_cartas);
-        let randej = bubbleSort(arr_numero);
-        console.log(cartas)
-        console.log(randej)
+        cantidad_glob.push(cantidad_cartas)
+        let mazo = array_cartas(cantidad_cartas);
+        mazo_glob.push(mazo)
+        for (let i = 0; i <= cantidad_cartas; i++ ) {
+            contenedor_cartas.appendChild(mazo[i])
+            console.log(mazo[i])
+        }
     });
 
+    boton_ordenador.addEventListener("click", function () {
+        bubbleSort.innerHTML = "";
+        let orden = bubbleSort(mazo_glob)
+        for (let i = 0; i <= cantidad_glob; i++ ) {
+            bubble_log.appendChild(mazo_glob[i])
+            console.log(orden[i])
+        }
+    })
+
     const bubbleSort = (cartas) => {
-        let wall = cartas.length - 1;
+        let orden = [...cartas]
+        let wall = orden.length - 1;
         while (wall > 0) {
             let index = 0;
             while (index < wall) {
-                if (cartas[index] > cartas[index + 1]) {
-                    let aux = cartas[index];
-                    cartas[index] = cartas[index + 1];
-                    cartas[index + 1] = aux;
+                if (orden[index][1] > orden[index + 1][1]) {
+                    let aux = orden[index];
+                    orden[index] = orden[index + 1];
+                    orden[index + 1] = aux;
                 }
                 index++;
             }
             wall--;
         }
-        return cartas;
+        console.log(orden)
+        return orden;
     };
 
 
-    boton_ordenador.addEventListener("click", function () {
-        let ordenando = Array_cartas_dadas(arr_numero,arr_pinta)
-    })
+
 
 }
